4bfe864d4ce3a786ae7754b916ce3e130e3c41d2
Clarison Dsilva <98853545+Clarison@users.noreply.github.com>
2023-09-19 16:07:52 -0400

    V1.1.23__otherscommit.sql

--- dbscripts/V1.1.22__othercommit.sql
+++
@@ -1,4 +0,0 @@
-create or replace view v1 as 
-(
-select author_id,first_name,last_name from authors
-);
---
+++ dbscripts/V1.1.23__otherscommit.sql
@@ -0,0 +1,4 @@
+create or replace view v1 as 
+(
+select author_id,first_name from authors
+);
